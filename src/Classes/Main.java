d3f4f6568db8a14ec6ce763ea546a48a21da2dcd
Deiner Rodrigues de Souza <deinersouza.tmsi@gmail.com>
2024-07-12 10:42:14 -0300

    Primeira versão

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -1,9 +1,23 @@
 package Classes;
-
+import java.util.Scanner;
 public class Main {
-
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//ler do console um valor numerico coloq	ue em uma variavel do tipo primitiva vassa conversão para o tipo wrapper 
+		
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe um numero\n");
+		long num1 = scan.nextLong();
+		
+		long variavel1 = num1;
+		
+		Short variavel2 = (short) variavel1;
+		
+		System.out.println("Casting e Wrappers concluido"+variavel2);
+		
 
 	}
 
